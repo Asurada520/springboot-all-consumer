63088af03f2d531ff19efcb8d3c2247d048eeac4
Asurada520 <602423940@qq.com>
2019-06-10 17:37:07 +0800

    add page test

--- src/main/java/com/ybzbcq/controller/InfoUserController.java
+++ src/main/java/com/ybzbcq/controller/InfoUserController.java
@@ -31,32 +31,62 @@ public class InfoUserController {
 
     @RequestMapping("getInfoById")
     @ResponseBody
-    public InfoUser userList(Integer id){
+    public InfoUser userList(Integer id) {
         InfoUser infoUser = userService.selectByPrimaryKey(id);
         return infoUser;
     }
 
     @RequestMapping("list")
-    public String getUserList(Model model,
-                              @RequestParam("currentPage") Integer currentPage, @RequestParam("pageSize") Integer pageSize){
+    public String getUserList(Model model, Integer currentPage) {
 
-        if(currentPage == null || currentPage < 1){
+        if (currentPage == null || currentPage < 1) {
             currentPage = 1;
         }
 
-        if(pageSize == null || currentPage < 1){
-            pageSize = 10;
+        Integer pageSize = 5;
+
+        if (pageSize == null || currentPage < 1) {
+            pageSize = 5;
+        }
+
+        int i = userService.qryCount(); // 查询数据总数
+        int totalPages = i / pageSize;
+
+        int left = i % pageSize;
+        if (left > 0) {
+            totalPages = totalPages + 1;
+        }
+
+
+        if(currentPage > totalPages){
+            currentPage = totalPages;
         }
 
         Map<String, Object> paramMap = new HashMap<String, Object>();
 
-        paramMap.put("currentPage", currentPage);
+        //  limit 开始行数
+        paramMap.put("currentPage", (currentPage - 1) * pageSize);
         paramMap.put("pageSize", pageSize);
         List<InfoUser> infoUsers = userService.selectByMultCondition(paramMap);
 
         model.addAttribute("infoUsers", infoUsers);
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("totalPages", totalPages);
 
         return "infoUser";
+    }
+
+    @RequestMapping("delete")
+    public void deleteUser(@RequestParam("id") Integer id){
+        if(!StringUtils.isEmpty(id)){
+            userService.deleteByPrimaryKey(id);
+        }
+    }
+
+
+    @RequestMapping("toAddUser")
+    public String toAddUser(){
+        return "addUser";
     }
 
 }
